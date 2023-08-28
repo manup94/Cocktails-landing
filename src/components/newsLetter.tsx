d3394b85f9ac2548f96f590e0bba0d230f4acf97
Manuel Pérez <mperezpradosjose@gmail.com>
2023-08-28 22:01:16 +0200

    ok

--- src/components/newsLetter.tsx
+++ src/components/newsLetter.tsx
@@ -5,11 +5,11 @@ export default function NewsLetter() {
             <h3 className="font-bold">Subscribete para las novedades y obtener ofertas unicas</h3>
             <form className="flex  m-5 text-black  flex-col items-center text-center" action="">
 
-                <label className="font-semibold" htmlFor="name">Nombre</label>
+                <label className="font-semibold text-white" htmlFor="name">Nombre</label>
                 <input className="rounded m-3 border-none text-center md:w-1/2" type="text" />
-                <label className="font-semibold" htmlFor="email"> Correo</label>
+                <label className="font-semibold text-white" htmlFor="email"> Correo</label>
                 <input className="rounded m-3 border-none text-center md:w-1/2" type="email" />
-                <label className="font-semibold" htmlFor="phone">Telefono</label>
+                <label className="font-semibold text-white" htmlFor="phone">Telefono</label>
                 <input className="rounded m-3 border-none text-center md:w-1/2" type="text" />
                 <button className="md:w-1/3 bg-black m-4 p-2 rounded-lg shadow-white text-white text-lg transition duration-300 shadow-xl transform hover:scale-90 focus:outline-none " type="button">
                     Enviar
